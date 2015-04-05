ed1e869c26ea345e20eb3fe73114a049e9f70db7
liulin2012 <liulin.jacob@gmail.com>
2015-04-05 16:46:02 -0400

    --wip--

---
+++ 15/LCS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void lcs(char* x, char* y){
+  
+}
+
+
+int main(int argc, char *argv[])
+{
+  char x[7]={'a','b','c','b','d','a','b'};
+  char y[6]={'b','d','c','a','b','a'};
+  lcs(x, y);
+  return 0;
+}
